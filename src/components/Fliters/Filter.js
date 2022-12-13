975fecc50719a98aeac9a4d16f76068508f5dad8
Kartik Singh Chuphal <2019009@iiitdmj.ac.in>
2022-12-13 17:27:36 +0530

    small change change none to all

--- src/components/Fliters/Filter.js
+++ src/components/Fliters/Filter.js
@@ -18,19 +18,19 @@ const Filter = ({area,setArea,category,setCategory,status,setStatus,areaOptions,
 			  <h5>FILTER BY</h5>
 				<FloatingLabel label="Area">
 					<Form.Select value={area} onChange={(e) => setArea(e.target.value)} className="mb-3">
-						<option key={-1} value={-1}>NONE</option>
+						<option key={-1} value={-1}>ALL</option>
 						{areaAvail}
 					</Form.Select>
 				</FloatingLabel>
 				<FloatingLabel label="Category">
 					<Form.Select value={category} onChange={(e) => setCategory(e.target.value)} className="mb-3">
-						<option key={-1} value={-1}>NONE</option>
+						<option key={-1} value={-1}>ALL</option>
 						{categoryAvail}
 					</Form.Select>
 				</FloatingLabel>
 				<FloatingLabel label="Status">
 					<Form.Select value={status} onChange={(e) => setStatus(e.target.value)} className="mb-3">
-						<option key={-1} value={-1}>NONE</option>
+						<option key={-1} value={-1}>ALL</option>
 						<option key={1} value={1}>OPEN</option>
 						<option key={2} value={2}>CLOSED</option>
  					</Form.Select>
